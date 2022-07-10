fcd00fb5e2b912d30b7e73b94fadfb2e60c117d2
Declan Morris <dtpmorris@gmail.com>
2022-07-10 07:47:23 +0100

    change protocol and destination for ssh rule

--- terraform/declanmorris.com.tf
+++ terraform/declanmorris.com.tf
@@ -97,11 +97,11 @@ resource "azurerm_network_security_rule" "SSHRule" {
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
-  protocol                    = "*"
+  protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
   source_address_prefix       = "*"
-  destination_address_prefix  = "*"
+  destination_address_prefix  = azurerm_network_interface.personalSiteNIC.private_ip_address
   resource_group_name         = azurerm_resource_group.personalSite.name
   network_security_group_name = azurerm_network_security_group.personalSiteNSG.name
 }

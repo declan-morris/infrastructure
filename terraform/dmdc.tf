1572ea97f48eac17f1cfdae6f82f6ad97516ef3b
Declan Morris <dtpmorris@gmail.com>
2022-08-08 20:54:33 +0100

    added wildcard cloudflare and fixed pve vars

--- terraform/dmdc.tf
+++ terraform/dmdc.tf
@@ -17,3 +17,11 @@ resource "cloudflare_record" "dmdcRootRecord" {
   type    = "A"
   proxied = false
 }
+
+resource "cloudflare_record" "dmdcWildcardRecord" {
+  zone_id = data.cloudflare_zone.dmdcZone.zone_id
+  name    = "*"
+  value   = azurerm_public_ip.pubIP.ip_address
+  type    = "A"
+  proxied = false
+}

d0bd6bef4aa631c3402e89ed9956d30a40f6c13d
Declan Morris <dtpmorris@gmail.com>
2022-08-24 21:40:56 +0100

    added other domain name to dmdc

--- terraform/dmdc.tf
+++ terraform/dmdc.tf
@@ -5,9 +5,9 @@ data "cloudflare_zone" "dmdcZone" {
 resource "cloudflare_record" "dmdcRecord" {
   zone_id = data.cloudflare_zone.dmdcZone.zone_id
   name    = "www"
-  value   = azurerm_public_ip.pubIP.ip_address
-  type    = "A"
-  proxied = false #todo set to true 
+  value   = "@"
+  type    = "CNAME"
+  proxied = true
 }
 
 resource "cloudflare_record" "dmdcRootRecord" {
@@ -15,11 +15,39 @@ resource "cloudflare_record" "dmdcRootRecord" {
   name    = "@"
   value   = azurerm_public_ip.pubIP.ip_address
   type    = "A"
-  proxied = false
+  proxied = true
 }
 
 resource "cloudflare_record" "dmdcWildcardRecord" {
   zone_id = data.cloudflare_zone.dmdcZone.zone_id
+  name    = "*"
+  value   = azurerm_public_ip.pubIP.ip_address
+  type    = "A"
+  proxied = true
+}
+
+data "cloudflare_zone" "declanmorris-com" {
+  name = "declanmorris.com"
+}
+
+resource "cloudflare_record" "declanmorris-comWWWRecord" {
+  zone_id = data.cloudflare_zone.declanmorris-com.zone_id
+  name    = "www"
+  value   = azurerm_public_ip.pubIP.ip_address
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "declanmorris-comRootRecord" {
+  zone_id = data.cloudflare_zone.declanmorris-com.zone_id
+  name    = "@"
+  value   = azurerm_public_ip.pubIP.ip_address
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "declanmorris-comWildcardRecord" {
+  zone_id = data.cloudflare_zone.declanmorris-com.zone_id
   name    = "*"
   value   = azurerm_public_ip.pubIP.ip_address
   type    = "A"

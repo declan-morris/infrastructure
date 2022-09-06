377e6f29da73d42408b85868a6527765e60f06f5
Declan Morris <dtpmorris@gmail.com>
2022-09-06 15:16:42 +0100

    tidy of soleria

--- ansible/roles/soleria/files/youtubedl/critRoleDownload.sh
+++ ansible/roles/soleria/files/youtubedl/critRoleDownload.sh
@@ -9,6 +9,7 @@ echo " starting run on $d"
 # TODO this may download too many videos, need to double check this!
 echo  "run youtube downloader for this playlist for videos uploaded in the last two weeks"
 /home/dtm/.local/bin/yt-dlp --dateafter today-2weeks --config-location /home/dtm/youtubedl/dl.config https://www.youtube.com/playlist?list=PL1tiwbzkOjQydg3QOkBLG9OYqWJ0dwlxF
+# /home/dtm/.local/bin/yt-dlp -I 15:17 --config-location /home/dtm/youtubedl/dl.config https://www.youtube.com/playlist?list=PL1tiwbzkOjQydg3QOkBLG9OYqWJ0dwlxF
 
 
 echo "get png files for each of the webp files"
@@ -27,8 +28,7 @@ done
 # get critical role id in sonarr
 id=$(curl --location --request GET 'http://soleria:8989/api/series/lookup?term=tvdb:296861&apikey=45adc9c9af2b44689c4f951300d7ff99' | jq -r '.[0].id')
 
-echo $id
-
+echo "the show id is: $id"
 echo "rescan the series for newly added files"
 curl --location --request POST 'http://soleria:8989/api/command?apikey=45adc9c9af2b44689c4f951300d7ff99' \
 --header 'Content-Type: application/json' \
@@ -39,6 +39,7 @@ curl --location --request POST 'http://soleria:8989/api/command?apikey=45adc9c9a
 
 sleep 10
 
+echo
 echo "rename the newly added files"
 curl --location --request POST 'http://soleria:8989/api/command?apikey=45adc9c9af2b44689c4f951300d7ff99' \
 --header 'Content-Type: application/json' \
@@ -50,4 +51,3 @@ curl --location --request POST 'http://soleria:8989/api/command?apikey=45adc9c9a
 ls -l "/storage/data/media/tvshows/Critical Role (2015)/Season 03/" | grep webm
 
 echo "finished #####################################"
-echo "\n\n\n"
